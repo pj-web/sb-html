ba48f1fc0ec3ba63bb1d294356658220ffdb54c2
pj-web <insta.pj.05@gmail.com>
2022-07-12 19:45:37 +0400

    20220711

--- app/js/script.js
+++ app/js/script.js
@@ -53,83 +53,6 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 
-    window.onload = function () {
-        const parallax = document.querySelector('.hero');
-    
-        if (parallax) {
-            const content = document.querySelector('.hero__container');
-            const clouds = document.querySelector('.images-hero__clouds');
-            const mountains = document.querySelector('.images-hero__mountains');
-            const human = document.querySelector('.images-hero__human');
-    
-            // Коэффициенты
-            const forClouds = 40;
-            const forMountains = 20;
-            const forHuman = 10;
-    
-            // Скорость анимации
-            const speed = 0.005;
-    
-            // Объявление переменных 
-            let positionX = 0, positionY = 0;
-            let coordXprocent = 0, coordYprocent = 0;
-    
-            function setMouseParallaxStyle() {
-                const distX = coordXprocent - positionX;
-                const distY = coordYprocent - positionY;
-    
-                positionX = positionX + (distX * speed);
-                positionY = positionY + (distY * speed);
-    
-                // Передаем стили
-                clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
-                mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
-                human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
-    
-                requestAnimationFrame(setMouseParallaxStyle);
-    
-            }
-            setMouseParallaxStyle();
-    
-            parallax.addEventListener("mousemove", function (e) {
-                // Получение ширины и высоты блока
-                const parallaxWidth = parallax.offsetWidth;
-                const parallaxHeight = parallax.offsetHeight;
-    
-                // Ноль по середине
-                const coordX = e.pageX - parallaxWidth / 2;
-                const coordY = e.pageY - parallaxHeight / 2;
-    
-                // Получаем проценты
-                coordXprocent = coordX / parallaxWidth * 100;
-                coordYprocent = coordY / parallaxHeight * 100;
-    
-            });
-    
-            // Parallax при скроле
-    
-            let thresholdSets = [];
-            for (let i = 0; i <=1.0; i += 0.005) {
-                thresholdSets.push(i);
-            }
-            const callback = function (entries, observer) {
-                const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
-                setParallaxItemsStyle(scrollTopProcent);
-            };
-            const observer = new IntersectionObserver(callback, {
-                threshold: thresholdSets
-            });
-    
-            observer.observe(document.querySelector('.description'));
-    
-            function setParallaxItemsStyle(scrollTopProcent) {
-                content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
-                // clouds.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 10}%);`;
-                mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
-                // human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
-            }
-        }
-    }
 }
 
 
@@ -140,15 +63,15 @@ if (isMobile.any()) {
 
 
 var prices = {
-	// RU: {
-	// 	country: 'Россия',
-	// 	country1: 'России',
-	// 	country2: 'России',
-	// 	old: '1499',
-	// 	new: '168',
-	// 	money: 'руб.',
-	// 	tel: '+7',
-	// },
+	RU: {
+		country: 'Россия',
+		country1: 'России',
+		country2: 'России',
+		old: '1499',
+		new: '147',
+		money: 'руб.',
+		tel: '+7',
+	},
 	KZ: {
 		country: 'Казахстан',
 		country1: 'Казахстана',
@@ -158,15 +81,15 @@ var prices = {
 		money: 'тенге',
 		tel: '+7',
 	},
-	BY: {
-		country: 'Беларусь',
-		country1: 'Беларуси',
-		country2: 'Беларуси',
-		old: '55',
-		new: '5',
-		money: 'BYN',
-		tel: '+375',
-	},
+	// BY: {
+	// 	country: 'Беларусь',
+	// 	country1: 'Беларуси',
+	// 	country2: 'Беларуси',
+	// 	old: '55',
+	// 	new: '5',
+	// 	money: 'BYN',
+	// 	tel: '+375',
+	// },
 };
 
 var changePrice = function changePrice(elems, value) {
